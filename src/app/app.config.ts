2bce072387a464ac33a983e49861936e14774669
Mykola Borkovskykh <mykola.borkovskykh@precisionformedicine.com>
2026-02-20 12:52:25 +0200

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-d1d9e","appId":"1:943796703431:web:887b13189237ded1dd2ee2","storageBucket":"friendlychat-d1d9e.firebasestorage.app","apiKey":"AIzaSyDMka0csHPgzc2MslHIKcTYka-YoNj_C-c","authDomain":"friendlychat-d1d9e.firebaseapp.com","messagingSenderId":"943796703431","measurementId":"G-X6WP6760QC","projectNumber":"943796703431","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging())
   ],
 };
